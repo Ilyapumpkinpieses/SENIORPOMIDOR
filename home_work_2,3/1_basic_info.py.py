89c3268fe791928205f36672be3b5ddc0afe57a4
Ilya <potashefff@mail.ru>
2025-03-11 15:58:25 +0300

    homewerok load

--- home_work_2,3/1_basic_info.py.py
+++ home_work_2,3/1_basic_info.py.py
@@ -1,1 +1,9 @@
-print("aloha")
+name=input("Введите Ваше имя:")
+experience= input("Сколько лет работаете в тестировщиком?:")
+question =("Что такое переменная?")
+print(question)
+answer =input ("Ваш ответ: ")
+if user_answer == "это именованная область памяти":
+    print(f"Ответ верный!")
+else :
+    print(f"Ответ неверный.")
