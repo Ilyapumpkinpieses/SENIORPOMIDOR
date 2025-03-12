44cd6b6cbd527fc9fc03c7cda408d7e4a8a9db85
Ilya <potashefff@mail.ru>
2025-03-13 01:27:56 +0300

    Slavno porabotali Friend

---
+++ Home_work_2.2/Zadaniya_iz_blokov/2.5.1_In_kilograms_grams.py
@@ -0,0 +1,3 @@
+grams=12345
+full_kg = grams // 1000
+print (f" В {grams} содержиться {full_kg} полных киллограм")
---
+++ Home_work_2.2/Zadaniya_iz_blokov/2.5.4.Ne_kratno_3.py
@@ -0,0 +1,13 @@
+number1 = 10
+number2 = 9
+result1 = number1 % 3
+result2 = number2 % 3
+if result1 != 0:
+    print (F"Число {number1} не кратно 3")
+else:
+    print(f"Число {number1}  кратно 3")
+
+if result2 != 0:
+    print (F"Число {number2} не кратно 3")
+else:
+    print(f"Число {number2}  кратно 3")
