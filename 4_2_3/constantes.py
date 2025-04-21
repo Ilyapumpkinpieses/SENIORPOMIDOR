16612b25662ccf425a8e0a1bcb5ed06426113962
Ilya <potashefff@mail.ru>
2025-04-21 17:41:41 +0300

    home

---
+++ 4_2_3/constantes.py
@@ -0,0 +1,19 @@
+BASE_URL = "https://api.pomidor-stage.ru"
+
+AUTH_HEADERS = {
+    "Content-Type": "application/x-www-form-urlencoded",
+    "Accept": "application/json"
+}
+
+API_HEADERS = {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+}
+
+AUTH_DATA = {
+    "username": "potasheffff@mail.ru.com",
+    "password": "sacura44",
+    "scope": "",
+    "client_id": "",
+    "client_secret": ""
+}
